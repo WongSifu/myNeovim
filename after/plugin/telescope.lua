38053f804635598fbcf32839d251f8c51a8cd727
Andrew Lim <write2andrewlim@gmail.com>
2022-12-28 22:00:17 -0500

    mapping

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -201,6 +201,7 @@ telescope.setup({
 local builtin = require("telescope.builtin")
 vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
 vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+vim.keymap.set("n", "<leader>gs", builtin.grep_string, {})
 vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
 vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
 vim.keymap.set("n", "<leader>fo", builtin.oldfiles, {})
