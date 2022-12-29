717eb9dd0ae4158bb13f665cfce02ecd94405fd6
Andrew Lim <write2andrewlim@gmail.com>
2022-12-28 22:57:59 -0500

    null ls

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -3,39 +3,38 @@ local lsp = require("lsp-zero")
 lsp.preset("recommended")
 
 lsp.ensure_installed({
-  'tsserver',
-  'eslint',
-  'sumneko_lua',
-  'gopls',
-  'volar',
-  'rust_analyzer',
+	"tsserver",
+	"eslint",
+	"sumneko_lua",
+	"gopls",
+	"volar",
+	"rust_analyzer",
 })
 
 -- Fix Undefined global 'vim'
-lsp.configure('sumneko_lua', {
-  settings = {
-    Lua = {
-      diagnostics = {
-        globals = { 'vim' }
-      }
-    }
-  }
+lsp.configure("sumneko_lua", {
+	settings = {
+		Lua = {
+			diagnostics = {
+				globals = { "vim" },
+			},
+		},
+	},
 })
 
 local status_ok, cmp = pcall(require, "cmp")
 if not status_ok then
-  print("could not find cmp")
-  return
+	print("could not find cmp")
+	return
 end
 
 local cmp_select = { behavior = cmp.SelectBehavior.Select }
 local cmp_mappings = lsp.defaults.cmp_mappings({
-  ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-  ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-  ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-  ["<C-Space>"] = cmp.mapping.complete(),
+	["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
+	["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
+	["<C-y>"] = cmp.mapping.confirm({ select = true }),
+	["<C-Space>"] = cmp.mapping.complete(),
 })
-
 
 -- disable completion with tab
 -- this helps with copilot setup
@@ -43,140 +42,133 @@ local cmp_mappings = lsp.defaults.cmp_mappings({
 --       print("fallback:", dump(fallback()))
 --       fallback()
 --     end)
-cmp_mappings['<Tab>'] = nil
-cmp_mappings['<S-Tab>'] = nil
-
+cmp_mappings["<Tab>"] = nil
+cmp_mappings["<S-Tab>"] = nil
 
 lsp.setup_nvim_cmp({
-  mapping = cmp_mappings,
+	mapping = cmp_mappings,
 })
 
-
 lsp.set_preferences({
-  suggest_lsp_servers = false,
-  sign_icons = {
-    error = 'E',
-    warn = 'W',
-    hint = 'H',
-    info = 'I'
-  }
+	suggest_lsp_servers = false,
+	sign_icons = {
+		error = "E",
+		warn = "W",
+		hint = "H",
+		info = "I",
+	},
 })
 
 -- For LSP Signature
 local cfg = {
-  debug = false, -- set to true to enable debug logging
-  log_path = vim.fn.stdpath("cache") .. "/lsp_signature.log", -- log dir when debug is on
-  -- default is  ~/.cache/nvim/lsp_signature.log
-  verbose = false, -- show debug line number
+	debug = false, -- set to true to enable debug logging
+	log_path = vim.fn.stdpath("cache") .. "/lsp_signature.log", -- log dir when debug is on
+	-- default is  ~/.cache/nvim/lsp_signature.log
+	verbose = false, -- show debug line number
 
-  bind = true, -- This is mandatory, otherwise border config won't get registered.
-  -- If you want to hook lspsaga or other signature handler, pls set to false
-  doc_lines = 10, -- will show two lines of comment/doc(if there are more than two lines in doc, will be truncated);
-  -- set to 0 if you DO NOT want any API comments be shown
-  -- This setting only take effect in insert mode, it does not affect signature help in normal
-  -- mode, 10 by default
+	bind = true, -- This is mandatory, otherwise border config won't get registered.
+	-- If you want to hook lspsaga or other signature handler, pls set to false
+	doc_lines = 10, -- will show two lines of comment/doc(if there are more than two lines in doc, will be truncated);
+	-- set to 0 if you DO NOT want any API comments be shown
+	-- This setting only take effect in insert mode, it does not affect signature help in normal
+	-- mode, 10 by default
 
-  max_height = 12, -- max height of signature floating_window
-  max_width = 80, -- max_width of signature floating_window
-  noice = false, -- set to true if you using noice to render markdown
-  wrap = true, -- allow doc/signature text wrap inside floating_window, useful if your lsp return doc/sig is too long
+	max_height = 12, -- max height of signature floating_window
+	max_width = 80, -- max_width of signature floating_window
+	noice = false, -- set to true if you using noice to render markdown
+	wrap = true, -- allow doc/signature text wrap inside floating_window, useful if your lsp return doc/sig is too long
 
-  floating_window = true, -- show hint in a floating window, set to false for virtual text only mode
+	floating_window = true, -- show hint in a floating window, set to false for virtual text only mode
 
-  floating_window_above_cur_line = true, -- try to place the floating above the current line when possible Note:
-  -- will set to true when fully tested, set to false will use whichever side has more space
-  -- this setting will be helpful if you do not want the PUM and floating win overlap
+	floating_window_above_cur_line = true, -- try to place the floating above the current line when possible Note:
+	-- will set to true when fully tested, set to false will use whichever side has more space
+	-- this setting will be helpful if you do not want the PUM and floating win overlap
 
-  floating_window_off_x = 1, -- adjust float windows x position.
-  floating_window_off_y = 0, -- adjust float windows y position. e.g -2 move window up 2 lines; 2 move down 2 lines
-  -- can be either number or function, see examples
+	floating_window_off_x = 1, -- adjust float windows x position.
+	floating_window_off_y = 0, -- adjust float windows y position. e.g -2 move window up 2 lines; 2 move down 2 lines
+	-- can be either number or function, see examples
 
-  close_timeout = 4000, -- close floating window after ms when laster parameter is entered
-  fix_pos = false, -- set to true, the floating window will not auto-close until finish all parameters
-  hint_enable = true, -- virtual hint enable
-  hint_prefix = "🐼 ", -- Panda for parameter, NOTE: for the terminal not support emoji, might crash
-  hint_scheme = "String",
-  hi_parameter = "LspSignatureActiveParameter", -- how your parameter will be highlight
-  handler_opts = {
-    border = "rounded" -- double, rounded, single, shadow, none, or a table of borders
-  },
+	close_timeout = 4000, -- close floating window after ms when laster parameter is entered
+	fix_pos = false, -- set to true, the floating window will not auto-close until finish all parameters
+	hint_enable = true, -- virtual hint enable
+	hint_prefix = "🐼 ", -- Panda for parameter, NOTE: for the terminal not support emoji, might crash
+	hint_scheme = "String",
+	hi_parameter = "LspSignatureActiveParameter", -- how your parameter will be highlight
+	handler_opts = {
+		border = "rounded", -- double, rounded, single, shadow, none, or a table of borders
+	},
 
-  always_trigger = false, -- sometime show signature on new line or in middle of parameter can be confusing, set it to false for #58
+	always_trigger = false, -- sometime show signature on new line or in middle of parameter can be confusing, set it to false for #58
 
-  auto_close_after = nil, -- autoclose signature float win after x sec, disabled if nil.
-  extra_trigger_chars = {}, -- Array of extra characters that will trigger signature completion, e.g., {"(", ","}
-  zindex = 200, -- by default it will be on top of all floating windows, set to <= 50 send it to bottom
+	auto_close_after = nil, -- autoclose signature float win after x sec, disabled if nil.
+	extra_trigger_chars = {}, -- Array of extra characters that will trigger signature completion, e.g., {"(", ","}
+	zindex = 200, -- by default it will be on top of all floating windows, set to <= 50 send it to bottom
 
-  padding = '', -- character to pad on left and right of signature can be ' ', or '|'  etc
+	padding = "", -- character to pad on left and right of signature can be ' ', or '|'  etc
 
-  transparency = nil, -- disabled by default, allow floating win transparent value 1~100
-  shadow_blend = 36, -- if you using shadow as border use this set the opacity
-  shadow_guibg = 'Black', -- if you using shadow as border use this set the color e.g. 'Green' or '#121315'
-  timer_interval = 200, -- default timer check interval set to lower value if you want to reduce latency
-  toggle_key = nil, -- toggle signature on and off in insert mode,  e.g. toggle_key = '<M-x>'
+	transparency = nil, -- disabled by default, allow floating win transparent value 1~100
+	shadow_blend = 36, -- if you using shadow as border use this set the opacity
+	shadow_guibg = "Black", -- if you using shadow as border use this set the color e.g. 'Green' or '#121315'
+	timer_interval = 200, -- default timer check interval set to lower value if you want to reduce latency
+	toggle_key = nil, -- toggle signature on and off in insert mode,  e.g. toggle_key = '<M-x>'
 
-  select_signature_key = nil, -- cycle to next signature, e.g. '<M-n>' function overloading
-  move_cursor_key = nil, -- imap, use nvim_set_current_win to move cursor between current win and floating
+	select_signature_key = nil, -- cycle to next signature, e.g. '<M-n>' function overloading
+	move_cursor_key = nil, -- imap, use nvim_set_current_win to move cursor between current win and floating
 }
 
-require 'lsp_signature'.setup(cfg) -- no need to specify bufnr if you don't use toggle_key
+require("lsp_signature").setup(cfg) -- no need to specify bufnr if you don't use toggle_key
 
 lsp.on_attach(function(client, bufnr)
-  local opts = { buffer = bufnr, remap = false }
+	local opts = { buffer = bufnr, remap = false }
 
-  if client.name == "eslint" then
-    vim.cmd.LspStop('eslint')
-    return
-  end
+	if client.name == "eslint" then
+		vim.cmd.LspStop("eslint")
+		return
+	end
 
-  -- if client.name == "volar" or client.name == "tsserver" then
-  if client.name == "tsserver" then
-    client.server_capabilities.documentFormattingProvider = false
-    client.server_capabilities.documentFormattingRangeProvider = false
-  end
+	-- if client.name == "volar" or client.name == "tsserver" then
+	if client.name == "tsserver" then
+		client.server_capabilities.documentFormattingProvider = false
+		client.server_capabilities.documentFormattingRangeProvider = false
+	end
 
-
-
-  -- vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-  vim.keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts)
-  vim.keymap.set("n", "gD", "<cmd>Telescope lsp_declarations<CR>", opts)
-  -- vim.keymap.set("n", "gI", vim.lsp.buf.implementation, opts)
-  vim.keymap.set("n", "gI", "<cmd>Telescope lsp_implementations<CR>", opts)
-  vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-  vim.keymap.set("n", "<leader>vws", vim.lsp.buf.workspace_symbol, opts)
-  vim.keymap.set("n", "<leader>vd", vim.diagnostic.open_float, opts)
-  vim.keymap.set("n", "[d", vim.diagnostic.goto_next, opts)
-  vim.keymap.set("n", "]d", vim.diagnostic.goto_prev, opts)
-  vim.keymap.set("n", "<leader>vca", vim.lsp.buf.code_action, opts)
-  -- vim.keymap.set("n", "<leader>lr", vim.lsp.buf.references, opts)
-  vim.keymap.set("n", "<leader>lr", "<cmd>Telescope lsp_references<CR>", opts)
-  vim.keymap.set("n", "<F2>", vim.lsp.buf.rename, opts)
-  vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, opts)
+	-- vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+	vim.keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts)
+	vim.keymap.set("n", "gD", "<cmd>Telescope lsp_declarations<CR>", opts)
+	-- vim.keymap.set("n", "gI", vim.lsp.buf.implementation, opts)
+	vim.keymap.set("n", "gI", "<cmd>Telescope lsp_implementations<CR>", opts)
+	vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+	vim.keymap.set("n", "<leader>vws", vim.lsp.buf.workspace_symbol, opts)
+	vim.keymap.set("n", "<leader>vd", vim.diagnostic.open_float, opts)
+	vim.keymap.set("n", "[d", vim.diagnostic.goto_next, opts)
+	vim.keymap.set("n", "]d", vim.diagnostic.goto_prev, opts)
+	vim.keymap.set("n", "<leader>vca", vim.lsp.buf.code_action, opts)
+	-- vim.keymap.set("n", "<leader>lr", vim.lsp.buf.references, opts)
+	vim.keymap.set("n", "<leader>lr", "<cmd>Telescope lsp_references<CR>", opts)
+	vim.keymap.set("n", "<F2>", vim.lsp.buf.rename, opts)
+	vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, opts)
 end)
-
 
 lsp.setup()
 
-
 --  NULL LS
-local null_ls = require('null-ls')
+local null_ls = require("null-ls")
 null_ls.setup({})
 
 local mason_nullls = require("mason-null-ls")
 mason_nullls.setup({
-  ensure_installed = { "prettier", "golangci-lint", "stylua", "jq", "black", "sql_formatter" },
-  automatic_installation = true,
-  automatic_setup = true,
+	ensure_installed = { "prettier", "golangci-lint", "stylua", "jq", "black", "sql_formatter" },
+	automatic_installation = true,
+	automatic_setup = true,
 })
 mason_nullls.setup_handlers({})
 
 vim.diagnostic.config({
-  virtual_text = true,
+	virtual_text = true,
 })
 
-
 -- Format on save
-vim.cmd([[autocmd BufWritePre * lua vim.lsp.buf.formatting_sync()]])
+vim.cmd([[autocmd BufWritePre * lua vim.lsp.buf.format()]])
 vim.keymap.set("n", "<leader>ft", vim.lsp.buf.format)
 
 -- recommended:
